801c279bad2a0a7741a47fd194ef714089224f2a
KevinKalb <kevin.kalb@aponia.de>
2023-08-16 22:29:52 +0200

    Code and tests work properly with only integer lists as input and easy sorting algorithms;

--- lib/consecutix.ex
+++ lib/consecutix.ex
@@ -4,15 +4,44 @@ defmodule Consecutix do
   """
 
   @doc """
-  Hello world.
+  Returns consecutively grouped elements.
+  Returned elements will be sorted.
+  Duplicated elements are deleted.
 
   ## Examples
 
-      iex> Consecutix.hello()
-      :world
+      iex> Consecutix.consecutive_lists([])
+      []
 
   """
-  def hello do
-    :world
+
+  @type output :: [list()]
+  @spec consecutive_lists(list(), fun()) :: output
+  def consecutive_lists(list, sorter \\ &Enum.sort/1)
+
+  def consecutive_lists(list, sorter) when is_function(sorter, 2) do
+    list |> Enum.sort(sorter) |> Enum.uniq() |> consecutive_lists_helper([], [])
+  end
+
+  def consecutive_lists(list, sorter) when is_function(sorter, 1) do
+    list |> sorter.() |> Enum.uniq() |> consecutive_lists_helper([], [])
+  end
+
+  defp consecutive_lists_helper([], [], result), do: Enum.reverse(result)
+
+  defp consecutive_lists_helper([], current_group, result),
+    do: Enum.reverse([Enum.reverse(current_group) | result])
+
+  defp consecutive_lists_helper([current | tail], [], result) do
+    consecutive_lists_helper(tail, [current], result)
+  end
+
+  defp consecutive_lists_helper([current | tail], [prev | _] = current_group, result)
+       when current - prev == 1 do
+    consecutive_lists_helper(tail, [current | current_group], result)
+  end
+
+  defp consecutive_lists_helper([current | tail], current_group, result) do
+    consecutive_lists_helper(tail, [current], [Enum.reverse(current_group) | result])
   end
 end
--- test/consecutix_test.exs
+++ test/consecutix_test.exs
@@ -2,7 +2,74 @@ defmodule ConsecutixTest do
   use ExUnit.Case
   doctest Consecutix
 
-  test "greets the world" do
-    assert Consecutix.hello() == :world
+  describe "simple tests" do
+    test "consecutive_lists/1 with empty list returns empty list" do
+      assert Consecutix.consecutive_lists([]) == []
+    end
+
+    test "consecutive_lists/1 with one element returns this element as a single group" do
+      assert Consecutix.consecutive_lists([1]) == [[1]]
+    end
+
+    test "consecutive_lists/1 with two identical elements returns both elements in the same group" do
+      assert Consecutix.consecutive_lists([1, 1]) == [[1]]
+    end
+
+    test "consecutive_lists/1 with two consecutive elements returns both elements in the same group" do
+      assert Consecutix.consecutive_lists([0, 1]) == [[0, 1]]
+    end
+
+    test "consecutive_lists/1 with two non-consecutive elements returns all elements in different groups" do
+      assert Consecutix.consecutive_lists([1, 3]) == [[1], [3]]
+    end
+
+    test "consecutive_lists/1 with three non-consecutive elements returns all elements in different groups" do
+      assert Consecutix.consecutive_lists([1, 3, 5]) == [[1], [3], [5]]
+    end
+
+    test "consecutive_lists/1 with three consecutive elements returns both elements in the same group" do
+      assert Consecutix.consecutive_lists([0, 1, 2]) == [[0, 1, 2]]
+    end
+
+    test "consecutive_lists/1 with four consecutive elements returns all elements in the same group" do
+      assert Consecutix.consecutive_lists([0, 1, 2, 3]) == [[0, 1, 2, 3]]
+    end
+
+    test "consecutive_lists/1 with four non-consecutive elements returns all elements in different groups" do
+      assert Consecutix.consecutive_lists([0, 2, 4, 6]) == [[0], [2], [4], [6]]
+    end
+
+    test "consecutive_lists/1 with 100 consecutive elements returns all elements in the same group" do
+      base = 0..99 |> Enum.into([])
+
+      assert Consecutix.consecutive_lists(base) == [base]
+    end
+
+    test "consecutive_lists/1 with 100 non-consecutive elements returns all elements in different groups" do
+      base = 0..199//2 |> Enum.into([])
+
+      assert Consecutix.consecutive_lists(base) == Enum.map(base, &[&1])
+    end
+  end
+
+  describe "more complex tests" do
+    test "consecutive_lists/1 sorts and parses properly" do
+      base = [11, 2, 0, 13, 22, 1, 14, 9]
+
+      assert Consecutix.consecutive_lists(base) == [[0, 1, 2], [9], [11], [13, 14], [22]]
+    end
+
+    test "consecutive_lists/2 sorts with parser as argument" do
+      base = [3, 1, 0]
+
+      assert Consecutix.consecutive_lists(base, &Enum.sort/1) == [[0, 1], [3]]
+    end
+
+    test "consecutive_lists/2 sorts with own sorter as argument" do
+      base = [3, 1, 0]
+      sorter = &(&1 < &2)
+
+      assert Consecutix.consecutive_lists(base, sorter) == [[0, 1], [3]]
+    end
   end
 end
