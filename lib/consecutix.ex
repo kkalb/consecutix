1c2307622323b51fe07b32b097d46509200ea1e2
KevinKalb <kevin.kalb@aponia.de>
2023-08-16 23:04:38 +0200

    added function head for consecutive check map

--- lib/consecutix.ex
+++ lib/consecutix.ex
@@ -13,18 +13,34 @@ defmodule Consecutix do
       iex> Consecutix.consecutive_lists([])
       []
 
+      iex> Consecutix.consecutive_lists([1])
+      [[1]]
+
+      iex> Consecutix.consecutive_lists([1, 2])
+      [[1, 2]]
+
+      iex> Consecutix.consecutive_lists([1, 3])
+      [[1], [3]]
+
   """
 
+  @spec consecutive_maps(list(map)) :: [list()]
+  def consecutive_maps([]) do
+    []
+  end
+
+  @type input :: list()
   @type output :: [list()]
-  @spec consecutive_lists(list(), fun()) :: output
+  @type sorter :: fun()
+  @spec consecutive_lists(input, sorter) :: output
   def consecutive_lists(list, sorter \\ &Enum.sort/1)
 
   def consecutive_lists(list, sorter) when is_function(sorter, 2) do
-    list |> Enum.sort(sorter) |> Enum.uniq() |> consecutive_lists_helper([], [])
+    list |> Enum.sort(sorter) |> consecutive_lists_helper([], [])
   end
 
   def consecutive_lists(list, sorter) when is_function(sorter, 1) do
-    list |> sorter.() |> Enum.uniq() |> consecutive_lists_helper([], [])
+    list |> sorter.() |> consecutive_lists_helper([], [])
   end
 
   defp consecutive_lists_helper([], [], result), do: Enum.reverse(result)
