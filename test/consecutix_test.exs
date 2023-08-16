99365ca2d98d9c53e0d3cb761edeb5376ef368dd
KevinKalb <kevin.kalb@aponia.de>
2023-08-16 23:04:48 +0200

    wrote some more specialized tests

--- test/consecutix_test.exs
+++ test/consecutix_test.exs
@@ -2,6 +2,7 @@ defmodule ConsecutixTest do
   use ExUnit.Case
   doctest Consecutix
 
+  @tag simple: true
   describe "simple tests" do
     test "consecutive_lists/1 with empty list returns empty list" do
       assert Consecutix.consecutive_lists([]) == []
@@ -52,6 +53,7 @@ defmodule ConsecutixTest do
     end
   end
 
+  @tag complex: true
   describe "more complex tests" do
     test "consecutive_lists/1 sorts and parses properly" do
       base = [11, 2, 0, 13, 22, 1, 14, 9]
@@ -70,6 +72,30 @@ defmodule ConsecutixTest do
       sorter = &(&1 < &2)
 
       assert Consecutix.consecutive_lists(base, sorter) == [[0, 1], [3]]
+    end
+  end
+
+  @tag usecase: true
+  describe "tests for some use cases" do
+    test "consecutive_lists/1 check integer in group" do
+      base = [0, 1, 5, 6, 8, 10, 11]
+      test_int = 6
+      # [[0, 1], [5, 6], [8], [10, 11]]
+      [g1, g2, g3, g4] = Consecutix.consecutive_lists(base)
+
+      assert 6 not in g1
+      assert 6 in g2
+      assert 6 not in g3
+      assert 6 not in g4
+    end
+  end
+
+  @tag special_data_types: true
+  describe "other data types" do
+    test "consecutive_lists/1 respects input data" do
+      base = [%{"int" => 0}, %{"int" => 1}, %{"int" => 2}]
+
+      assert Consecutix.consecutive_maps(base) == [[%{"int" => 0}, %{"int" => 1}, %{"int" => 2}]]
     end
   end
 end
